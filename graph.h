a05a2f84e342da39a14b45747bc879362ea550e4
Wendy Ide <ide.s.wendy@gmail.com>
2018-11-15 20:05:14 -0500

    more pseudocode

--- graph.h
+++ graph.h
@@ -6,14 +6,26 @@ class Graph{
     public:
         Graph();
         void dijkstra(node s);
-    
+        void load();
+        void output();  
+
     private:
         class Node; //edge needs t recognize node
-        class Edge{};
-        class Node{};
+        class Edge{
+        	//next node
+        	//weight
+        };
+        class Node{
+            public: 
+            list<Edge> adj; //adjacency list
+            bool known;
+            DistType dist; //DIstType is probably int
+            Vertex path; //Probably vertext *, as mentioned above 
+            //other datat member functions as needed
+        };
 
-};
+};r
 
-
+//heap delete min (shortest path)
 
 #endif //_GRAPH_H
