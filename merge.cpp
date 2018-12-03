9dd9abb8917dba31dac5ae755b00c4cd7981ae73
Wendy Ide <ide.s.wendy@gmail.com>
2018-12-03 16:35:03 -0500

    PRESUB 2 to Sable

--- merge.cpp
+++ merge.cpp
@@ -15,7 +15,7 @@ int main(){
     std::string inFile_name, outFile_name; 
     std::string stringA, stringB,stringC;
     int lenA, lenB, lenC;
-    int j, i; 
+    int i, j;  //for backtracking later
 
     std::cout << "Enter name of input file: ";
     std::cin >> inFile_name;
@@ -29,7 +29,6 @@ int main(){
         ifs >> stringA >> stringB >> stringC;
         if (ifs.eof()) //makes sure last line doesn't duplicate
             break;    
-         std::cout << stringA << " " <<  stringB << " " << stringC << std::endl; //testing, del later
         lenA = stringA.length();
         lenB = stringB.length();
         lenC = stringC.length();
@@ -40,73 +39,45 @@ int main(){
          }
         
         //matrix is i,j indexed    
-        for (int i=0; i<lenA+1; i++){ //fill in matrix
+        for (int i=0; i<lenA+1; i++){ //fill in boolean matrix
             for (int j=0; j<lenB+1; j++){
-                arr[i][j] = false; 
+                arr[i][j] = false;   //preset to false
                 if (i==0 && j==0) 
                     arr[i][j] = true; 
-                //A is empty 
-                else if (i==0 && stringB[j-1]==stringC[j-1]) 
+                else if (i==0 && stringB[j-1]==stringC[j-1]) //A is empty 
                     arr[i][j] = arr[i][j-1]; 
-                //B is empty 
-                else if (j==0 && stringA[i-1]==stringC[i-1]) 
+                else if (j==0 && stringA[i-1]==stringC[i-1]) //B is empty 
                     arr[i][j] = arr[i-1][j]; 
-                //Current character of C matches with current character of A, 
-                //but doesn't match with current character of B 
-                else if(stringA[i-1]==stringC[i+j-1] && stringB[j-1]!=stringC[i+j-1]) 
+                else if(stringA[i-1]==stringC[i+j-1] && stringB[j-1]!=stringC[i+j-1]) //current C char matches current A char, but not current B char
                     arr[i][j] = arr[i-1][j]; 
-                //Current character of C matches with current character of B, 
-                //but doesn't match with current character of A 
-                else if (stringA[i-1]!=stringC[i+j-1] && stringB[j-1]==stringC[i+j-1]) 
+                else if (stringA[i-1]!=stringC[i+j-1] && stringB[j-1]==stringC[i+j-1]) //C matches B, but not A
                     arr[i][j] = arr[i][j-1]; 
-                //Current character of C matches with that of both A and B 
-                else if (stringA[i-1]==stringC[i+j-1] && stringB[j-1]==stringC[i+j-1]) 
+                else if (stringA[i-1]==stringC[i+j-1] && stringB[j-1]==stringC[i+j-1]) //C matches both A and B
                     arr[i][j]=(arr[i-1][j] || arr[i][j-1]) ; 
-                    
             }
         }
         
-        
-   //testbench, del later
-      for (int i = 0; i < lenA+1; ++i){
-        for (int j = 0; j < lenB+1; ++j){
-            std::cout << arr[i][j] << ' ';
-        }
-        std::cout << std::endl;
-    }
-
         //backtrack to get CAPs placement for stringA
         i = lenA;  
         j = lenB;
-          std::cout << "orig: " << (arr[i][j])<< std::endl; 
-           std::cout << "orig i,j: " << i << ", " << j<< std::endl;
         if(arr[i][j]){  //last matrix entry indicates if merge is valid (1 if valid)
             while(i>=0 && j>=0){
                 if (i==0 && j==0)
                     break;
-                if (arr[i][j-1]){  //try going left first
-                    std::cout << "(arr[i][j-1]: " << (arr[i][j-1])<< std::endl;
-                    std::cout << "goingleft" << std::endl;
+                if (arr[i][j-1])  //try going left first
                     j--;
-                }
                 else if (arr[i-1][j]){   //then try going left (makes sure CAPs takes farthest left (first) availble spot for StringA)
-                    std::cout << "(arr[i-1][j]: " << (arr[i-1][j])<< std::endl;
-                    std::cout << "goingup" << std::endl;
                     stringC[i+j-1] = toupper(stringC[i+j-1]); //changes stringC in place
                     i--;      
                 }
                 
             }
-             std::cout << "stgC fixed: " << stringC << std::endl; //testing, del later
             ofs << stringC << std::endl; 
         }
-        else if (!arr[i][j]){
-           std::cout<< "*** NOT A MERGE ***" << std::endl; //testing, del late
+        else if (!arr[i][j])
             ofs << "*** NOT A MERGE ***" << std::endl; 
-        }
     
     } //end of big while loop
-
 
     ofs.close(); //close the output file
     return 0;
